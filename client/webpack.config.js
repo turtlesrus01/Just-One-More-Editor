a366a69dd81aa001a272629deb8d2cf16f2d48ab
Green Camper <stefan.palacios@gmail.com>
2023-06-07 23:02:00 -0500

    Added service worker code to webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
-const { InjectManifest } = require("workbox-webpack-plugin");
+const { InjectManifest, GenerateSW } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // : Add CSS loaders and babel to webpack.
@@ -25,6 +25,7 @@ module.exports = () => {
         template: "./index.html",
         title: "Webpack Plugin",
       }),
+      new GenerateSW(),
     ],
     module: {
       rules: [
